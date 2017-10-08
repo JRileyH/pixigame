7cdcbac7b9feb961217ab2367df6ea6434c53c92
James Harrington <riggy@megalon.home>
2017-10-08 18:17:27 -0400

    togglable animation

--- src/engine/manifest/player.js
+++ src/engine/manifest/player.js
@@ -6,8 +6,10 @@ class Player extends require('./entity'){
             if(this._sprite_type==='spine'){
                 if(this._animation_state==='idle'){
                     this._sprite.state.setAnimation(0, animation, true);
+                    this._animation_state=animation;
                 } else {
                     this._sprite.state.setAnimation(0, 'idle', true);
+                    this._animation_state='idle';
                 }
                 
             }
