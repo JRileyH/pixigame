8952c6ceb744e5132db23676db44ac4192529849
James Harrington <JRileyHarrington@gmail.com>
2017-11-19 13:59:30 -0500

    expanded modals to have minimization

--- src/engine/toolbox/ui/components/modal.js
+++ src/engine/toolbox/ui/components/modal.js
@@ -3,22 +3,22 @@ class Modal extends require('../ui-component'){
         super(u, options);
         this._background = new PIXI.Sprite();
         this._background.x = 0;
-        this._background.y = 0;
+        this._background.y = 30;
         this._background.width = this._bounds._width;
-        this._background.height = this._bounds._height;
+        this._background.height = this._bounds._height-30;
         this._background.texture = !!options.background ? options.background : PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAMzMzP///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==');
 
-        if(!Array.isArray(this._options.components)) this._options.components = [];
+        if(!Array.isArray(this._options.components)) { this._components = [];} else { this._components = this._options.components.slice()}
 
         this._dragbar = this._ui.Button(text,{
-            bounds: new PIXI.Rectangle(0, 0, this._bounds._width-30, 30),
+            bounds: new PIXI.Rectangle(0, 0, this._bounds._width-60, 30),
             texture: {
-                plain: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAP+IAP///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='),
-                hover: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAP+qIv///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='),
-                click: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAMx3AP///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==')
+                plain: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAJmZ3f///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='),
+                hover: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAALu7/////yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='),
+                click: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAHd3u////yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==')
             },
         }).create(this);
-        this._closebutton = this._ui.Button('X',{
+        this._closebutton = this._ui.Button(' X',{
             bounds: new PIXI.Rectangle(this._bounds._width-30, 0, 30, 30),
             texture: {
                 plain: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAP9mZv///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='),
@@ -26,7 +26,24 @@ class Modal extends require('../ui-component'){
                 click: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAP9ERP///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==')
             },
             click: ()=>{
-                this.destroy();
+                this.close();
+            }
+        }).create(this);
+
+        this._collapsed = false;
+        this._minbutton = this._ui.Button('  -',{
+            bounds: new PIXI.Rectangle(this._bounds._width-60, 0, 30, 30),
+            texture: {
+                plain: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAP+IAP///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='),
+                hover: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAP+qIv///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='),
+                click: PIXI.Texture.from('data:image/gif;base64,R0lGODlhAQABAPAAAMx3AP///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==')
+            },
+            click: ()=>{
+                if(this._collapsed){
+                    this.expand();
+                } else {
+                    this.collapse();
+                }
             }
         }).create(this);
 
@@ -57,11 +74,33 @@ class Modal extends require('../ui-component'){
     }
     create(parent) {
         super.create(parent);
-        this._bounds.addChild(this._background, this._dragbar.Bounds, this._closebutton.Bounds);
-        for(let component of this._options.components){
+        this._bounds.addChild(this._background, this._dragbar.Bounds, this._closebutton.Bounds, this._minbutton.Bounds);
+        for(let component of this._components){
             this._bounds.addChild(component.create(this).Bounds)
         }
         return this;
+    }
+
+    close(){
+        this.destroy();
+    }
+    collapse(){
+        if(!this._collapsed){
+            this._background.visible = false;
+            for(let component of this._components){
+                component.Bounds.visible = false;
+            }
+            this._collapsed=true;
+        }
+    }
+    expand(){
+        if(this._collapsed){
+            this._background.visible = true;
+            for(let component of this._components){
+                component.Bounds.visible = true;
+            }
+            this._collapsed=false;
+        }
     }
 }
 
